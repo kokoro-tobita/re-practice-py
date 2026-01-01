1f3ba60f4ba3b84847d0232829b8c14312d90a66
kokoro-hart <tobita@wd-flat.com>
2026-01-01 17:01:24 +0900

    feat: number.py

---
+++ number.py
@@ -0,0 +1,17 @@
+import math
+
+print(2 + 2)
+print(5 - 2)
+print(2 * 2)
+print(10 / 2)
+print(17 // 3)
+print(10 % 3)
+print(10 ** 2)
+print(type(1))
+print(type(1.6))
+
+num = 3.14159265359
+print(round(num, 2))
+
+result = math.sqrt(25)
+print(result)

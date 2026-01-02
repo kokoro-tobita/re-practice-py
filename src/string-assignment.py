475a1882072397e1e22868690809ea9d1028334b
kokoro-hart <tobita@wd-flat.com>
2026-01-02 17:13:13 +0900

    feat: string-assignment.py

---
+++ src/string-assignment.py
@@ -0,0 +1,17 @@
+temp = 'a is {}'.format('a')
+print(temp)
+
+temp = 'a is {} and b is {}'.format('a', 'b')
+print(temp)
+
+temp = 'a is {0} and b is {1}'.format('a', 'b')
+print(temp)
+
+temp = 'a is {0} and b is {1} and c is {0}'.format('a', 'b')
+print(temp)
+
+temp = 'a is {0} and b is {1} and c is {0}'.format('a', 'b')
+print(temp)
+
+temp = 'my name is {name} and I am {age} years old'.format(name='John', age=30)
+print(temp)

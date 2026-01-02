2b73e6dcea538401b79eb0f0f0d41d62f44d5a6d
kokoro-hart <tobita@wd-flat.com>
2026-01-03 01:43:27 +0900

    feat: set

---
+++ src/set.py
@@ -0,0 +1,7 @@
+a = {1, 2, 2, 3, 4, 4, 5}
+b = {2, 3, 3, 4, 4, 5, 6}
+
+print(a - b)
+print(a & b)
+print(a | b)
+print(a ^ b)
---
+++ src/set_implementation.py
@@ -0,0 +1,9 @@
+my_friends = {'A', 'B', 'C'}
+A_friends = {'B', 'D', 'E', 'F'}
+B_friends = {'E', 'G', 'H'}
+
+print(my_friends & A_friends)
+
+fruits = ['apple', 'banana', 'apple', 'banana']
+kind = set[str](fruits)
+print(kind)

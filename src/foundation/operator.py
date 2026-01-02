20a15bfa236c73ec058ca4e5d18857094f2412ba
kokoro-hart <tobita@wd-flat.com>
2026-01-03 02:04:39 +0900

    feat: operator

---
+++ src/foundation/operator.py
@@ -0,0 +1,29 @@
+a = 2
+b = 2
+
+print(a == b)
+
+print(a != b)
+
+print(a < b)
+
+print(a > b)
+
+print(a <= b)
+
+print(a >= b)
+
+print(a > 0 and b > 0)
+
+print(a > 0 or b > 0)
+
+print(not a > 0)
+
+if(a > 0 and b > 0):
+    print('a and b are positive')
+
+if(a > 0 or b > 0):
+    print('a or b are positive')
+
+if not(a > 0):
+    print('a is not positive')
